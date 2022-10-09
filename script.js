b2a69ea10926ca320561ae10c98482c1b9ec97b3
asbhogal <asbhogal1@live.co.uk>
2022-10-09 17:24:03 +0100

    added hide-show icon toggle

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const toggleShowOrHidePWD = document.querySelector('#password-show-or-hide-section');
 let passwordState = false;
+let hideOrShowIcon = document.querySelector('.fa-eye');
 
 toggleShowOrHidePWD.addEventListener('click', () => {
     if (passwordState) {
         document.getElementById("password-entry").setAttribute("type", "password");
+        hideOrShowIcon.classList.add('fa-eye');
+        hideOrShowIcon.classList.remove('fa-eye-slash');
         passwordState = false;
+        
     } else {
         document.getElementById("password-entry").setAttribute("type", "text");
+        hideOrShowIcon.classList.add('fa-eye-slash')
+        hideOrShowIcon.classList.remove('fa-eye');
         passwordState = true;
     }
 })
